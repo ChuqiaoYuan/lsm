c45c41a52641365a1c6edd0b679b6d9e83a6eeb0
chy131 <chy131@chy131deMacBook-Air.local>
2020-05-04 22:31:46 -0400

    Update client-server mode

---
+++ client.c
@@ -0,0 +1,51 @@
+//主要任务是读完workload.txt然后把每一行逐条发给server，并且读server的回执，当每一条都收到回执后再关上
+
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#define PORT 8080 
+#define SA struct sockaddr 
+
+void query(int sockfd){
+	char buff[80];
+	char result[1000];
+	FILE *fp = fopen("workload.txt", "rt");
+	while(!feof(fp)){
+		fgets(buff, 80, fp);
+		write(sockfd, buff, sizeof(buff));
+		bzero(buff, sizeof(buff));
+		read(sockfd, result, sizeof(result));
+		printf("Result from server %s\n", result);
+	}
+	fclose(fp);
+}
+
+int main(){
+	int sockfd, connfd;
+	struct sockaddr_in servaddr, cli;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd == -1){
+		printf("Socket creation failed.\n");
+		exit(0);
+	}else{
+		printf("Socket successfully created.\n");
+	}
+	bzero(&servaddr, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    servaddr.sin_port = htons(PORT);
+
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+        printf("connection with the server failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("connected to the server..\n"); 
+
+    query(sockfd);
+    close(sockfd);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,168 @@
+//6个operation都要写，每个operation的command line看265DSL
+
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include "lsm.h"
+#define PORT 8080 
+#define SA struct sockaddr 
+  
+void response(int sockfd){ 
+    char buff[80]; 
+    char result[1000];
+    LSMtree *CreateLSM()
+
+    while (1){
+    	bzero(buff, 80);
+    	read(sockfd, buff, sizeof(buff));
+    	printf("Query from client %s\n", buff);
+
+    	if(buff[0] == 'p'){
+    		int pos = 2;
+    		int key = 0;
+    		int sign = 1;
+    		if(buff[pos] == '-'){
+    			sign = -1;
+    			pos += 1;
+    		}
+    		while((buff[pos] >= '0') && (buff[pos] <= '9')){
+    			key = key * 10 + (buff[pos] - '0');
+    			pos += 1;
+    		}
+    		key = key * sign;
+
+    		int value = 0;
+    		pos += 1;
+    		sign = 1;
+    		if(buff[pos] == '-'){
+    			sign = -1;
+    			pos += 1;
+    		}
+    		while((buff[pos] >= '0') && (buff[pos] <= '9')){
+    			value = value * 10 + (buff[pos] - '0');
+    			pos += 1;
+    		}
+    		value = value * sign;
+    		printf("key %d value %d \n", key, value);
+    	}else if(buff[0] == 'g'){
+    		int pos = 2;
+    		int key = 0;
+    		int sign = 1;
+    		if(buff[pos] == '-'){
+    			sign = -1;
+    			pos += 1;
+    		}
+    		while((buff[pos] >= '0') && (buff[pos] <= '9')){
+    			key = key * 10 + (buff[pos] - '0');
+    			pos += 1;
+    		}
+    		key = key * sign;
+    		printf("key %d \n", key);
+    	}else if(buff[0] == 'r'){
+    		int pos = 2;
+    		int start = 0;
+    		int sign = 1;
+    		if(buff[pos] == '-'){
+    			sign = -1;
+    			pos += 1;
+    		}
+    		while((buff[pos] >= '0') && (buff[pos] <= '9')){
+    			start = start * 10 + (buff[pos] - '0');
+    			pos += 1;
+    		}
+    		start = start * sign;
+
+    		int end = 0;
+    		pos += 1;
+    		sign = 1;
+    		if(buff[pos] == '-'){
+    			sign = -1;
+    			pos += 1;
+    		}
+    		while((buff[pos] >= '0') && (buff[pos] <= '9')){
+    			end = end * 10 + (buff[pos] - '0');
+    			pos += 1;
+    		}
+    		end = end * sign;
+    		printf("start %d end %d \n", start, end);
+    	}else if(buff[0] == 'd'){
+    		int pos = 2;
+    		int key = 0;
+    		int sign = 1;
+    		if(buff[pos] == '-'){
+    			sign = -1;
+    			pos += 1;
+    		}
+    		while((buff[pos] >= '0') && (buff[pos] <= '9')){
+    			key = key * 10 + (buff[pos] - '0');
+    			pos += 1;
+    		}
+    		key = key * sign;
+    		printf("key %d \n", key);
+    	}else if(buff[0] == 'l'){
+    		printf("%c", buff[2]);
+    	}else if(buff[0] == 's'){
+    		printf("ook");
+    	}
+
+    	bzero(result, 1000);
+
+    	result[0] = 'a';
+    	result[1] = 'n';
+    	result[2] = 'n';
+    	//printf("Response to client %s\n", result);
+    	write(sockfd, result, sizeof(result));
+    }
+} 
+  
+int main(){ 
+    int sockfd, connfd, len; 
+    struct sockaddr_in servaddr, cli; 
+  
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1){ 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    }else{
+        printf("Socket successfully created..\n"); 
+    }
+    bzero(&servaddr, sizeof(servaddr)); 
+  
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(PORT); 
+  
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0){ 
+        printf("socket bind failed...\n"); 
+        exit(0); 
+    } 
+    else{
+        printf("Socket successfully binded..\n"); 
+    }
+  
+    if ((listen(sockfd, 5)) != 0){ 
+        printf("Listen failed...\n"); 
+        exit(0); 
+    } 
+    else{
+        printf("Server listening..\n"); 
+    }
+    len = sizeof(cli); 
+  
+    connfd = accept(sockfd, (SA*)&cli, &len); 
+    if (connfd < 0) { 
+        printf("server acccept failed...\n"); 
+        exit(0); 
+    } 
+    else{
+        printf("server acccept the client...\n"); 
+    }
+  
+    response(connfd); 
+    close(sockfd); 
+    return 0;
+} 
